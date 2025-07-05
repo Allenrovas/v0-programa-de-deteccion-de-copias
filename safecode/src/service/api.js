8f9059ff3b2857cb360d9888b62748005a3dc293
Allen Román <2909080001301@ingenieria.usac.edu.gt>
2025-07-04 22:08:39 -0600

    Fix enviroment Frontend

--- safecode/src/service/api.js
+++ safecode/src/service/api.js
@@ -2,7 +2,7 @@
 import ApiService from './apiService';
 
 // Crea una instancia única de ApiService con la URL base de tu API
-const api = new ApiService(import.meta.env.VITE_API_URL || 'http://localhost:8000');
+const api = new ApiService(`http://${import.meta.env.VITE_BACKEND_HOST || 'localhost'}:${import.meta.env.VITE_BACKEND_PORT || 8000}`);
 
 // Exporta la instancia para que pueda ser utilizada en toda la aplicación
 export default api;
